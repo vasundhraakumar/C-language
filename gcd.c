ae1a10c271fb12cc9355c748b25d7c8642cd8330
vasundhraa <110687612+vasundhraakumar@users.noreply.github.com>
2023-07-21 14:09:44 +0530

    Add files via upload

---
+++ gcd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int gcd(int a,int b)
+{
+    if(b==0)
+        return(a);
+    else
+        return(gcd(b,a%b));
+}
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    //gcd(a,b);
+    printf("%d\n",gcd(a,b));
+    int lcm=(a*b)/gcd(a,b);
+    printf("%d",lcm);
+}
